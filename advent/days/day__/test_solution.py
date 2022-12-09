c375fb4ad5a4a995c12446b52936769b29fc457a
Ruediger Ludwig <github@savinien.de>
2022-12-09 07:41:47 +0100

    small adjustment to template

--- advent/days/day__/test_solution.py
+++ advent/days/day__/test_solution.py
@@ -4,14 +4,14 @@ from .solution import day_num, part1, part2
 
 
 def test_part1():
-    data = utils.read_data(day_num, 'test01.txt')
+    lines = utils.read_data(day_num, 'test01.txt')
     expected = None
-    result = part1(data)
+    result = part1(lines)
     assert result == expected
 
 
 def test_part2():
-    data = utils.read_data(day_num, 'test01.txt')
+    lines = utils.read_data(day_num, 'test01.txt')
     expected = None
-    result = part2(data)
+    result = part2(lines)
     assert result == expected
