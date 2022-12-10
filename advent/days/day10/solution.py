7d0d3e504e9abba9c40badf1670958f7498d6984
Ruediger Ludwig <github@savinien.de>
2022-12-10 08:16:14 +0100

    Remove unneeded class (thanks to meowmeowwarrior)

--- advent/days/day10/solution.py
+++ advent/days/day10/solution.py
@@ -6,57 +6,58 @@ day_num = 10
 
 
 def part1(lines: Iterator[str]) -> int:
-    return sum(Signal.grab_values(lines))
+    return sum(grab_values(lines))
 
 
 def part2(lines: Iterator[str]) -> list[str]:
-    return Signal.draw(lines, 40, 6)
+    return draw(lines, 40, 6)
 
 
-class Signal:
-    @staticmethod
-    def parse(line: str) -> None | int:
-        """
-        Parses the a line into the two possible instructions.
-        May raise if the instructions was invalid
-        """
-        match line.split():
-            case ['noop']:
-                return None
-            case ['addx', value]:
-                return int(value)
-            case _:
-                raise Exception(f"Unknown line: {line}")
+def parse(line: str) -> None | int:
+    """
+    Parses the a line into the two possible instructions.
+    May raise if the instructions was invalid
+    """
+    match line.split():
+        case ['noop']:
+            return None
+        case ['addx', value]:
+            return int(value)
+        case _:
+            raise Exception(f"Unknown line: {line}")
 
-    @staticmethod
-    def cycles(lines: Iterator[str]) -> Iterator[int]:
-        """
-        Cycles throw the instructions and yields a new value on each cycle
-        """
-        register = 1
-        for line in lines:
-            yield register
-            command = Signal.parse(line)
-            if command is not None:
+
+def cycles(lines: Iterator[str]) -> Iterator[int]:
+    """
+    Cycles through the instructions and yields a new value for each cycle
+    """
+    register = 1
+    for line in lines:
+        yield register
+        match parse(line):
+            case None:
+                pass
+            case value:
                 yield register
-                register += command
+                register += value
+    yield register
 
-    @staticmethod
-    def grab_values(lines: Iterator[str]) -> Iterator[int]:
-        for cycle, value in enumerate(Signal.cycles(lines), start=1):
-            if cycle in [20, 60, 100, 140, 180, 220]:
-                yield cycle * value
 
-    @staticmethod
-    def draw(lines: Iterator[str], width: int, height: int) -> list[str]:
-        picture = ""
-        for cycle, sprite in enumerate(Signal.cycles(lines)):
-            crt_pos = cycle % width
-            if sprite - 1 <= crt_pos and crt_pos <= sprite + 1:
-                picture += '#'
-            else:
-                picture += ' '
+def grab_values(lines: Iterator[str]) -> Iterator[int]:
+    for cycle, value in enumerate(cycles(lines), start=1):
+        if cycle in [20, 60, 100, 140, 180, 220]:
+            yield cycle * value
 
-            if crt_pos == width - 1:
-                picture += '\n'
-        return picture.split('\n')[:height]
+
+def draw(lines: Iterator[str], width: int, height: int) -> list[str]:
+    picture = ""
+    for cycle, sprite in enumerate(cycles(lines)):
+        crt_pos = cycle % width
+        if sprite - 1 <= crt_pos and crt_pos <= sprite + 1:
+            picture += '#'
+        else:
+            picture += ' '
+
+        if crt_pos == width - 1:
+            picture += '\n'
+    return picture.split('\n')[:height]
